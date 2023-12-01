6575055eedbf33efbce9bc8a2f482402f4b55a68
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2023-12-01 11:18:34 +0530

    Update NavLinks.js

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -15,12 +15,14 @@ const NavLinks = () => {
             <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="https://sites.google.com/view/ab-infotech-about-us/home">
                 Contact Us
             </Link>
+            /*
             // <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/enquiry">
             //     Enquiry
             // </Link>
             // <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo">
             //     Demo our products
             // </Link>
+            */
         </>
     )
 }
