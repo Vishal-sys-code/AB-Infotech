6d5ccc5513d1565297a2626ca6a7dbaaea5f99f1
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2023-12-01 11:14:40 +0530

    Update NavLinks.js

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -6,21 +6,21 @@ const NavLinks = () => {
     const url1 = 'https://sites.google.com/view/ab-infotech-info/home';
     return (
         <>
-            <Link href={url1} className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/about">
+            <Link href={url1} className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="https://sites.google.com/view/ab-infotech-info/home?pli=1">
                 About
             </Link>
-            <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/services">
+            <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="https://sites.google.com/view/ab-infotech-services/home">
                 Services
             </Link>
-            <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/portfolio">
-                Portfolio
+            <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="https://sites.google.com/view/ab-infotech-about-us/home">
+                Contact Us
             </Link>
-            <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/enquiry">
-                Enquiry
-            </Link>
-            <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo">
-                Demo our products
-            </Link>
+            // <Link className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/enquiry">
+            //     Enquiry
+            // </Link>
+            // <Link className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo">
+            //     Demo our products
+            // </Link>
         </>
     )
 }
